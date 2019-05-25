12478d4a2edcb156121b818522c160808be62d08
xtianmarucco <christian.marucco@gmail.com>
2019-05-25 04:52:54 -0300

    falta servicios, terminar contacto, y ajustar el css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent} from './pages/about/about.component';
+import { BannerComponent } from './pages/banner/banner.component';
+import { ContactComponent } from './pages/contact/contact.component'
+ 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    BannerComponent,
+    ContactComponent
+    
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
